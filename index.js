5d5b0fbf9c82cef6f61c03ce529021db4cf60a0e
Juan Mendoza <juan.j.mendoza513@gmail.com>
2022-10-27 02:10:07 -0400

    function to check if guess is correct or wrong

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 const buttons = document.querySelector(".squares");
 const rows = buttons.children;
+let wordOfTheDay = ""; // assigned in getWordOfTheDay(), it's an immediately invoked function expression.
 
 let currentRowIndex = 0;
 let currentSquareIndex = 0;
 
-let currentWord = [];
+let guessedChars = [];
 
 document.addEventListener("keydown", function (event) {
   let row = rows[currentRowIndex % 6].children;
   let square = row[currentSquareIndex % 5];
 
-  getWordOfTheDay().then(async (wordOfTheDay) => {
-    console.log(wordOfTheDay);
-  });
-
   if (isLetter(event.key) && currentRowIndex < 6) {
     square.innerText = event.key;
-    if (currentWord.length == 5) {
-      currentWord.pop();
+    if (guessedChars.length == 5) {
+      guessedChars.pop();
     }
-    currentWord.push(event.key);
+    guessedChars.push(event.key);
     if (currentSquareIndex % 5 < 4) {
       currentSquareIndex += 1;
     }
-  } else if (event.key == "Enter" && currentWord.length == 5) {
+  } else if (event.key == "Enter" && guessedChars.length == 5) {
     // check if word is valid with api, if so
     if (true) {
       // go to the next row
       currentRowIndex += 1;
       currentSquareIndex = 0;
-      currentWord = [];
+      guessedChars = [];
     }
   } else if (event.key == "Backspace" && currentSquareIndex > 0) {
     if (row[currentSquareIndex].innerText == "") {
       currentSquareIndex -= 1;
     }
-    currentWord.pop();
+    guessedChars.pop();
     row[currentSquareIndex].innerText = "";
   }
 });
@@ -46,8 +43,17 @@ function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
 
-async function getWordOfTheDay() {
+(async function getWordOfTheDay() {
   const promise = await fetch("https://words.dev-apis.com/word-of-the-day");
   const processedResponse = await promise.json();
-  return processedResponse.word;
+  wordOfTheDay = processedResponse.word;
+  return wordOfTheDay;
+})();
+
+function isCorrectGuess(guessedChars) {
+  const guessWord = guessedChars.join("");
+  if (guessWord == wordOfTheDay) {
+    return true;
+  }
+  return false;
 }
