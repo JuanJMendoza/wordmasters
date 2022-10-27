56a1e87ab57361b593fd293f602df7e3d41ef485
Juan Mendoza <juan.j.mendoza513@gmail.com>
2022-10-27 01:19:07 -0400

    Handle backspace behavior

--- index.js
+++ index.js
@@ -15,24 +15,28 @@ document.addEventListener("keydown", function (event) {
 
   if (isLetter(event.key) && currentRowIndex < 6) {
     square.innerText = event.key;
+    if (currentWord.length == 5) {
+      currentWord.pop();
+    }
     currentWord.push(event.key);
     if (currentSquareIndex % 5 < 4) {
       currentSquareIndex += 1;
     }
-  } else if (event.key == "Enter" && currentSquareIndex % 5 == 4) {
+  } else if (event.key == "Enter" && currentWord.length == 5) {
     // check if word is valid with api, if so
     if (true) {
       // go to the next row
       currentRowIndex += 1;
       currentSquareIndex = 0;
-    } else {
-      // otherwise
-      // flash all squares with red borders and fade back to gray
+      currentWord = [];
     }
-  } /* else if (event.key == "Backspace" && currentSquareIndex >= 0) {
-    row[currentSquareIndex % 5].innerText = "";
-    currentSquareIndex = currentSquareIndex < 0 ? 0 : (currentSquareIndex -= 1);
-  } */
+  } else if (event.key == "Backspace" && currentSquareIndex > 0) {
+    if (row[currentSquareIndex].innerText == "") {
+      currentSquareIndex -= 1;
+    }
+    currentWord.pop();
+    row[currentSquareIndex].innerText = "";
+  }
 });
 
 // Utility Functions
